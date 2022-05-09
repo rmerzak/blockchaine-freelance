82af8f47ef197e01a4a55496805cc644ece8dffc
rmerzak <rmerzak@student.1337.ma>
2022-05-09 22:58:10 +0100

    working on contract backend

--- contract/delance.sol
+++ contract/delance.sol
@@ -3,26 +3,45 @@ pragma solidity 0.6.4;
 pragma experimental ABIEncoderV2;
 
 contract Delance {
-    address public employer;
-    address public freelancer;
-    uint public deadline;
-    uint public price;
 
-    struct Request {
-        string title;
+    enum Status {COMPLETED, CENCLED, PENDING}
+    
+    address     payable public  employer;
+    address     payable public  freelancer;
+    uint256     public          deadline;
+    uint256     public          price;
+    uint256     public          remainingPayments;
+    uint256     public          createdAt;
+    Status      public          status;
+
+    struct  Request {
+        string  title;
         uint256 amount;
-        bool loked;
-        bool paid;
+        bool    loked;
+        bool    paid;
     }
 
-    Request[] public requests;
+    Request[]   public  requests;
 
-    constructor (address _freelancer, uint _deadline) public payable {
-        employer = msg.sender;
+    event   RequestUnlocked(bool loked);
+    event   RequestCreated(string title, uint256 amount, bool locked, bool paid);
+    event   RequestPaid(address receiver, uint256 amount);
+
+    constructor (
+        address payable _freelancer, 
+        address payable _employer, 
+        uint256 _deadline, 
+        uint256 _price
+        ) public {
+        employer = _employer;
         freelancer = _freelancer;
         deadline = _deadline;
-        price = msg.value;
+        createdAt = now;
+        price = _price;
+        remainingPayments = _price;
+        status = Status.PENDING;
     }
+
     receive() external payable {
         price += msg.value;
     }
@@ -32,18 +51,52 @@ contract Delance {
         _;
     }
 
-    function createRequest(string memory _title, uint256 _amount) public {
+    modifier onlyEmployer() {
+        require(msg.sender == employer, "onlyEmployer");
+        _;
+    }
+
+    modifier onlyPendingProject() {
+        require(status == Status.PENDING, "Only Pending");
+        _;
+    }
+
+
+    function createRequest(string memory _title, uint256 _amount) public onlyFreelancer {
         Request memory request = Request (
             {
                 title: _title,
                 amount: _amount,
                 loked: true,
-                paid:false
+                paid: false
             }
         );
         requests.push(request);
+
+        emit RequestCreated(_title, _amount, request.loked, request.paid);
     }
+
     function getAllRequests() public view returns(Request[] memory) {
         return requests;
+    }
+
+    function unlockRequest(uint256 _index) public onlyEmployer {
+        Request storage request = requests[_index];
+        require(request.loked, "Already unlocked");
+        request.loked = false;
+
+        emit RequestUnlocked(request.loked);
+    }
+
+    function payRequest(uint256 _index) public onlyEmployer onlyPendingProject {
+        Request storage request = requests[_index];
+        require(!request.loked,"Request is locked");
+        require(!request.paid, "already paid");
+
+        remainingPayments -= request.amount;
+        freelancer.transfer(request.amount);
+        request.paid = true;
+        emit    RequestPaid(msg.sender, request.amount);
+
     }
 }
